0cf246d96b80c2da4802c44c6a7ad073f02ddd28
Peter Simon <simonpepe@gmail.com>
2015-06-12 13:19:17 +0200

    Add gulp and webpack for building the app

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// CSS
+require('normalize-css/normalize.css');
+require('base.less/dist/base.css');
+require('../styles/main.css');
+require('../styles/score.css');
+require('../styles/board.css');
+
+// JS
+require('babel/polyfill');
---
+++ gulpconfig.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var webpack = require('webpack');
+
+var config = {};
+
+// Configurable paths
+// Don't use leading or trailing slashes!
+config.path = {
+    app: 'app',
+    dist: 'dist'
+};
+
+// Plugins preferences
+config.plugins = {
+    // Bumps the version number (and create a git commit and tag)
+    bump: {
+        packageFiles: [ 'package.json', 'bower.json' ]
+    },
+
+    minifyHtml: {
+        removeComments: true,
+        removeCommentsFromCDATA: true,
+        removeCDATASectionsFromCDATA: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true
+    },
+
+    // http://webpack.github.io/docs/configuration.html
+    webpack: {
+        context: __dirname + '/' + config.path.app,
+        entry: './scripts/main.js',
+        output: {
+            path: __dirname + '/' + config.path.dist + '/assets/',
+            filename: 'bundle.js',
+            publicPath: 'assets/'
+        },
+        resolve: {
+            extensions: [ '', '.js' ],
+            modulesDirectories: [
+                'node_modules', config.path.app + '/bower_components'
+            ]
+        },
+        plugins: [
+            new webpack.ResolverPlugin(
+                new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(
+                    'bower.json', [ 'main' ]
+                )
+            )
+        ],
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loaders: [ 'babel-loader' ]
+                }, {
+                    test: /\.css$/,
+                    loaders: [
+                        'style-loader',
+                        'css-loader',
+                        'autoprefixer-loader?browsers=last 2 version'
+                    ]
+                }, {
+                    test: /\.(png|jpg|svg)$/,
+                    loaders: [ 'url-loader?limit=8192' ]
+                }
+            ]
+        }
+    }
+};
+
+module.exports = config;
