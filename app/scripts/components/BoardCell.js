6edcd8a8a8d1ca2320b7ff7b8b44cee3afe373bc
Peter Simon <simonpepe@gmail.com>
2015-06-17 11:06:30 +0200

    Animate cell value change

--- app/scripts/components/BoardCell.js
+++ app/scripts/components/BoardCell.js
@@ -10,6 +10,11 @@ define([ 'react' ], function(React) {
 
     return React.createClass({
         displayName: 'BoardCell',
+        getInitialState: function() {
+            return {
+                value: this.props.value
+            };
+        },
         getDefaultProps: function() {
             return {
                 value: EMPTY_DISK
@@ -18,9 +23,15 @@ define([ 'react' ], function(React) {
         propTypes: {
             value: React.PropTypes.oneOf([ EMPTY_DISK, LIGHT_DISK, DARK_DISK ])
         },
+        handleClick: function(e) {
+            e.preventDefault();
+            let opposite = (this.state.value === LIGHT_DISK) ?
+                DARK_DISK : LIGHT_DISK;
+            this.setState({ value: opposite });
+        },
         render: function() {
             let disk;
-            switch (this.props.value) {
+            switch (this.state.value) {
             case EMPTY_DISK:
                 disk = '';
                 break;
@@ -32,7 +43,11 @@ define([ 'react' ], function(React) {
                 break;
             }
 
-            return DOM.div({ className: 'col', 'data-disk': disk });
+            return DOM.div({
+                className: 'col',
+                'data-disk': disk,
+                onClick: this.handleClick
+            });
         }
     });
 });
