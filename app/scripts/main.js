a1cabce3f17276b3e20b4b920bd17efe44e08995
Peter Simon <simonpepe@gmail.com>
2015-06-18 11:03:51 +0200

    Update base.less to v0.4.0

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@
 
 // CSS
 require('normalize-css/normalize.css');
-require('base.less/dist/base.css');
+require('base.less/dist/css/base.css');
 require('../styles/main.css');
 require('../styles/score.css');
 require('../styles/board.css');
